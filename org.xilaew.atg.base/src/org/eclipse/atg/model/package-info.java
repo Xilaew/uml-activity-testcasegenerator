b9653d8ea1b16094089e7e0566282e1c1e416f4f
xilaew@gmail.com <xilaew@gmail.com>
2013-07-24 21:07:14 +0200

    striped base plugin to base

--- org.xilaew.atg.base/src/org/eclipse/atg/model/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * All subpackages hereof are Generated from ecore models. This Package contains some Handwritten Classes working with the generated Modelcode
- */
-/**
- * @author th51e0
- *
- */
-package org.eclipse.atg.model;
