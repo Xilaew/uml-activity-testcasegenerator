b68b3c1e7db9eee0e54e5a22e66d6704b5086433
xilaew@gmail.com <xilaew@gmail.com>
2014-01-13 02:39:44 +0100

    fixed bug in Binary Counter example source code

--- umlModels/ActivityExamples/AcademicProblems/SAT/Class1.c
+++ umlModels/ActivityExamples/AcademicProblems/SAT/Class1.c
@@ -5,6 +5,7 @@
  *      Author: TH51E0
  */
 #include "Class1.h"
+#include <stdio.h>
 
 bool s0;
 bool s1;
@@ -14,11 +15,12 @@ bool s3;
 void count(const bool* const o0, const bool* const o1,
 		const bool* const o2, const bool* const o3) {
 	do {
-		s0=~s0;
-		s1 = s0? s1:~s1;
-		s2 = s0||s1? s2:~s2;
-		s3 = s0||s1||s2? s3:~s3;
-	} while (*o0!=s0 && *o1!=s1 && *o2!=s2 && *o3!=s3);
+		s0= !s0;
+		s1 = s0? s1:!s1;
+		s2 = s0||s1? s2:!s2;
+		s3 = s0||s1||s2? s3:!s3;
+	} while (*o0!=s0 || *o1!=s1 || *o2!=s2 || *o3!=s3);
+	printf("%d %d %d %d\n",s0,s1,s2,s3);
 }
 
 
